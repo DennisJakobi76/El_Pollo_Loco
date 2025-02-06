d7f32a345efd42aac9feed7e86edd1404f0e34c8
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-02-06 18:31:51 +0100

    added backgoundObjects and did refactoring at drawing objects on canvas

--- models/world.class.js
+++ models/world.class.js
@@ -2,6 +2,7 @@ class World {
     character = new Character();
     enemies = [new Chicken(), new Chicken(), new Chicken()];
     clouds = [new Cloud(), new Cloud()];
+    backgroundObjects = [new BackgroundObject("../assets/img/5_background/layers/1_first_layer/1.png", 0, 280)];
     canvas;
     ctx;
 
@@ -13,9 +14,17 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        this.enemies.forEach((enemy) => this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height));
-        this.clouds.forEach((cloud) => this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height));
+        this.addToMap(this.character);
+        this.addObjectsFromArrayToMap(this.enemies);
+        this.addObjectsFromArrayToMap(this.clouds);
+        this.addObjectsFromArrayToMap(this.backgroundObjects);
         requestAnimationFrame(this.draw.bind(this));
+    }
+
+    addObjectsFromArrayToMap(array) {
+        array.forEach((object) => this.addToMap(object));
+    }
+    addToMap(movableObject) {
+        this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height);
     }
 }
