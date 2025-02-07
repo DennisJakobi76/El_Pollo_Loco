213ee76d7424506d3bd72b57ae71a3bf01471e26
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-02-07 11:38:25 +0100

    animated clouds and character

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,33 @@
 class Character extends MovableObject {
+    height;
+    width;
+    y;
+    IMAGES_WALKING = [
+        "../assets/img/2_character_pepe/2_walk/W-21.png",
+        "../assets/img/2_character_pepe/2_walk/W-22.png",
+        "../assets/img/2_character_pepe/2_walk/W-23.png",
+        "../assets/img/2_character_pepe/2_walk/W-24.png",
+        "../assets/img/2_character_pepe/2_walk/W-25.png",
+        "../assets/img/2_character_pepe/2_walk/W-26.png",
+    ];
+    currentImage = 0;
     constructor() {
         super().loadImage("../assets/img/2_character_pepe/2_walk/W-21.png");
+        this.loadImages(this.IMAGES_WALKING);
+        this.height = 240;
+        this.width = 124;
+        this.y = 480 - this.height * 1.22;
+        this.animate();
     }
+
+    animate() {
+        setInterval(() => {
+            let index = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[index];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
+    }
+
     jump() {}
 }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,9 +1,9 @@
 class Chicken extends MovableObject {
     constructor() {
         super().loadImage("../assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
-        this.height = 50;
-        this.width = 50;
+        this.height = 72;
+        this.width = 72;
         this.x = 200 + Math.random() * 500;
-        this.y = 480 - this.height * 2;
+        this.y = 480 - this.height * 1.8;
     }
 }
