31eaf881c4e2025a35d51608f765212ad6fe74ca
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-02-07 17:18:55 +0100

    added level

---
+++ levels/level-1.js
@@ -0,0 +1,21 @@
+enemies = [new Chicken(), new Chicken(), new Chicken()];
+clouds = [new Cloud()];
+backgroundObjects = [];
+
+addBackgroundObjectsToArray(20, backgroundObjects, -719);
+
+let level1 = new Level(enemies, clouds, backgroundObjects);
+
+function addBackgroundObjectsToArray(screenCounter, array, worldSize) {
+    for (let i = 0; i < screenCounter; i++) {
+        array.push(new BackgroundObject("../assets/img/5_background/layers/air.png", worldSize));
+        array.push(new BackgroundObject("../assets/img/5_background/layers/3_third_layer/1.png", worldSize));
+        array.push(new BackgroundObject("../assets/img/5_background/layers/2_second_layer/1.png", worldSize));
+        array.push(new BackgroundObject("../assets/img/5_background/layers/1_first_layer/1.png", worldSize));
+        array.push(new BackgroundObject("../assets/img/5_background/layers/air.png", worldSize + 719));
+        array.push(new BackgroundObject("../assets/img/5_background/layers/3_third_layer/2.png", worldSize + 719));
+        array.push(new BackgroundObject("../assets/img/5_background/layers/2_second_layer/2.png", worldSize + 719));
+        array.push(new BackgroundObject("../assets/img/5_background/layers/1_first_layer/2.png", worldSize + 719));
+        worldSize = worldSize + 1438;
+    }
+}
