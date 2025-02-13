f78d3b79b63b442917e3fa96b937ef2089093284
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-02-13 10:27:39 +0100

    improved throw function with random landed bottle on the ground

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,6 +1,17 @@
 class ThrowableObject extends MovableObject {
+    IMAGES_ROTATION = [
+        "../assets/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+        "../assets/img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+        "../assets/img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+        "../assets/img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+    ];
+
+    IMAGES_BOTTLE_ON_GROUND = ["../assets/img/6_salsa_bottle/1_salsa_bottle_on_ground.png", "../assets/img/6_salsa_bottle/2_salsa_bottle_on_ground.png"];
+
     constructor(x, y) {
-        super().loadImage("../assets/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png");
+        super().loadImage(this.IMAGES_ROTATION[0]);
+        this.loadImages(this.IMAGES_BOTTLE_ON_GROUND);
+        this.loadImages(this.IMAGES_ROTATION);
         this.x = x;
         this.y = y;
         this.height = 60;
@@ -11,8 +22,26 @@ class ThrowableObject extends MovableObject {
     throw() {
         this.fallSpeed = 15;
         this.applyGravity();
-        setInterval(() => {
-            this.isAboveGround() ? (this.x += 10) : (this.x += 0);
+        this.intervalId = setInterval(() => {
+            if (this.isAboveGround()) {
+                this.playFlyingBottleAnimation();
+            } else {
+                this.playBottleOnGroundAnimation();
+            }
         }, 30);
+    }
+
+    playFlyingBottleAnimation() {
+        this.playAnimationInfinite(this.IMAGES_ROTATION);
+        this.x += 10;
+    }
+
+    playBottleOnGroundAnimation() {
+        const randomIndex = Math.floor(Math.random() * this.IMAGES_BOTTLE_ON_GROUND.length);
+        this.playAnimationOnce([this.IMAGES_BOTTLE_ON_GROUND[randomIndex]]);
+        this.x += 0;
+        setTimeout(() => {
+            clearInterval(this.intervalId);
+        }, 60);
     }
 }
