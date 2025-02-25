e8a95259d08c2e47625b84268d30f3a987323088
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-02-25 15:27:17 +0100

    set correct pathes in level js

--- levels/level-1.js
+++ levels/level-1.js
@@ -46,14 +46,14 @@ function initLevel() {
 
 function addBackgroundObjectsToArray(screenCounter, array, worldSize) {
     for (let i = 0; i < screenCounter; i++) {
-        array.push(new BackgroundObject("../assets/img/5_background/layers/air.png", worldSize));
-        array.push(new BackgroundObject("../assets/img/5_background/layers/3_third_layer/1.png", worldSize));
-        array.push(new BackgroundObject("../assets/img/5_background/layers/2_second_layer/1.png", worldSize));
-        array.push(new BackgroundObject("../assets/img/5_background/layers/1_first_layer/1.png", worldSize));
-        array.push(new BackgroundObject("../assets/img/5_background/layers/air.png", worldSize + 719));
-        array.push(new BackgroundObject("../assets/img/5_background/layers/3_third_layer/2.png", worldSize + 719));
-        array.push(new BackgroundObject("../assets/img/5_background/layers/2_second_layer/2.png", worldSize + 719));
-        array.push(new BackgroundObject("../assets/img/5_background/layers/1_first_layer/2.png", worldSize + 719));
+        array.push(new BackgroundObject("assets/img/5_background/layers/air.png", worldSize));
+        array.push(new BackgroundObject("assets/img/5_background/layers/3_third_layer/1.png", worldSize));
+        array.push(new BackgroundObject("assets/img/5_background/layers/2_second_layer/1.png", worldSize));
+        array.push(new BackgroundObject("assets/img/5_background/layers/1_first_layer/1.png", worldSize));
+        array.push(new BackgroundObject("assets/img/5_background/layers/air.png", worldSize + 719));
+        array.push(new BackgroundObject("assets/img/5_background/layers/3_third_layer/2.png", worldSize + 719));
+        array.push(new BackgroundObject("assets/img/5_background/layers/2_second_layer/2.png", worldSize + 719));
+        array.push(new BackgroundObject("assets/img/5_background/layers/1_first_layer/2.png", worldSize + 719));
         worldSize = worldSize + 1438;
     }
 }
