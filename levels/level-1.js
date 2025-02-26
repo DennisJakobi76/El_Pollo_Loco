e5f189cd2a550276024ef728e23b7c017633bf46
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-02-26 10:55:37 +0100

    bugfixes for replay all intervalls start correct clouds are added to world again and bottles are drawn correct

--- levels/level-1.js
+++ levels/level-1.js
@@ -1,47 +1,54 @@
-let level1;
-
-enemies = [
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-    new SmallChicken(),
-    new SmallChicken(),
-    new SmallChicken(),
-    new SmallChicken(),
-    new Endboss(),
-];
-clouds = [
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-    new Cloud(),
-];
-
-coins = [new CollectableObject(), new CollectableObject(), new CollectableObject(), new CollectableObject(), new CollectableObject()];
-
-backgroundObjects = [];
-
 function initLevel() {
+    level1 = null;
+    cloudCounter = 1;
+    chickenCounter = 1;
+    coinCounter = 1;
+    enemies = [];
+    clouds = [];
+    coins = [];
+    backgroundObjects = [];
+
+    enemies = [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new SmallChicken(),
+        new SmallChicken(),
+        new SmallChicken(),
+        new SmallChicken(),
+        new Endboss(),
+    ];
+    clouds = [
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+    ];
+
+    coins = [new CollectableObject(), new CollectableObject(), new CollectableObject(), new CollectableObject(), new CollectableObject()];
+
     addBackgroundObjectsToArray(4, backgroundObjects, -719);
     level1 = new Level(enemies, clouds, backgroundObjects, coins);
+
+    return level1;
 }
 
 function addBackgroundObjectsToArray(screenCounter, array, worldSize) {
