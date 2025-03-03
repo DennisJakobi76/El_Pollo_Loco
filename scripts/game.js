f0c579a8c33f8a6481d623d7155884af7a3300c7
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-03-03 11:44:24 +0100

    added mobile control functionality

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,8 @@
+const CONTROL_LEFT = document.getElementById("character-control-left");
+const CONTROL_RIGHT = document.getElementById("character-control-right");
+const CONTROL_JUMP = document.getElementById("character-control-jump");
+const CONTROL_THROW = document.getElementById("character-control-throw");
+
 let canvas;
 let world;
 let keyboard = new Keyboard();
@@ -10,6 +15,42 @@ function createWorld() {
     world = null;
     world = new World(canvas, keyboard);
 }
+
+CONTROL_LEFT.addEventListener("touchstart", (event) => {
+    event.preventDefault();
+    keyboard.LEFT = true;
+});
+CONTROL_LEFT.addEventListener("touchend", (event) => {
+    event.preventDefault();
+    keyboard.LEFT = false;
+});
+
+CONTROL_RIGHT.addEventListener("touchstart", (event) => {
+    event.preventDefault();
+    keyboard.RIGHT = true;
+});
+CONTROL_RIGHT.addEventListener("touchend", (event) => {
+    event.preventDefault();
+    keyboard.RIGHT = false;
+});
+
+CONTROL_JUMP.addEventListener("touchstart", (event) => {
+    event.preventDefault();
+    keyboard.UP = true;
+});
+CONTROL_JUMP.addEventListener("touchend", (event) => {
+    event.preventDefault();
+    keyboard.UP = false;
+});
+
+CONTROL_THROW.addEventListener("touchstart", (event) => {
+    event.preventDefault();
+    keyboard.D = true;
+});
+CONTROL_THROW.addEventListener("touchend", (event) => {
+    event.preventDefault();
+    keyboard.D = false;
+});
 
 window.addEventListener("keydown", (event) => {
     if (event.keyCode == 39) {
