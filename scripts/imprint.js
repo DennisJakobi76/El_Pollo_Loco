42e7c45c502aabf1650e9e32e9cff6294f61ec84
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-03-03 10:50:33 +0100

    added fullscreen icon to imprint and story

--- scripts/imprint.js
+++ scripts/imprint.js
@@ -1,3 +1,11 @@
 function getBackToPreviousSite() {
     window.history.back();
 }
+
+function toggleFullscreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
+}
